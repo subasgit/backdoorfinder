d166d5c06332fde0e3be8b6f22ae97e793fa8364
subasgit <subadevi@gmail.com>
2020-09-23 18:31:28 -0700

    Added functionality to store apikey and file path in a separate file as a one time input. The main script will depend on fetching the input from configure.txt.

--- src/generate_backdoor_report.py
+++ src/generate_backdoor_report.py
@@ -5,8 +5,20 @@ def write_to_csv_processes_exposed_network_attack():
     """ Find processes that is listening on 0.0.0.0 and exposing ur network for attack and write it in csv file """
     process_list = backdoor.processes_exposed_network_attack()
     print(process_list)
+    with open("configure.txt", 'r') as f1:
+        for line in f1:
+            if "file path" in line:
+                file_location = line.split("= ", 1)[1]
+            else:
+                value = "none"
+                file_location = "default"
     # Write it to CSV file
-    backdoor.convert_to_csv('process_exposed_network_attack.csv', process_list)
+    if file_location != "default":
+        final_file_path = file_location+"/process_exposed_network_attack.csv"
+    else:
+        final_file_path = "process_exposed_network_attack.csv"
+
+    backdoor.convert_to_csv(final_file_path, process_list)
     print("Processes exposed to network attacks are written in process_exposed_network_attack.csv")
 
 
@@ -16,11 +28,24 @@ def write_to_csv_suspicious_process_to_unknown_ports():
     API VOID and provide the API key to cross verify if the IP address is really malicious.\
     You can skip that check if you don't want to cross check the maliciousness of the IP by typing none """
     # Create a new API key in API VOID and enter it here. If you don't want to check, enter none
-    api_key = input("Enter your api_key: ")
-    process_list = backdoor.suspicious_process_to_unknown_ports(api_key)
+    with open("configure.txt", 'r') as f1:
+        for line in f1:
+            if "api_key" in line:
+                value = line.split("=", 1)[1]
+            elif "file path" in line:
+                file_location = line.split("= ", 1)[1]
+            else:
+                value = "none"
+                file_location = "default"
+    process_list = backdoor.suspicious_process_to_unknown_ports(value)
     print(process_list)
     # Write it to CSV file
-    backdoor.convert_to_csv('suspicious_process_to_unknown_ports.csv', process_list)
+    if file_location != "default":
+        final_file_path = file_location+"/suspicious_process_to_unknown_ports.csv"
+    else:
+        final_file_path = "suspicious_process_to_unknown_ports.csv"
+
+    backdoor.convert_to_csv(final_file_path, process_list)
     print("Suspicious process connecting to unknown ports are written in suspicious_process_to_unknown_ports.csv ")
 
 
